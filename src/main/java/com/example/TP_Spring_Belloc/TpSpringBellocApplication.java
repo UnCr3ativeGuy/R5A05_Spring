44ad27ef410716daea82ab48bbeee6c555ceacc2
AdrienBelloc <adrien82200@gmail.com>
2024-11-21 15:11:19 +0100

    Création du projet

---
+++ src/main/java/com/example/TP_Spring_Belloc/TpSpringBellocApplication.java
@@ -0,0 +1,19 @@
+package com.example.TP_Spring_Belloc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class TpSpringBellocApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(TpSpringBellocApplication.class, args);
+	}
+	@GetMapping("/hello")
+	public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
+		return String.format("Hello %s!", name);
+	}
+}
---
+++ src/test/java/com/example/TP_Spring_Belloc/TpSpringBellocApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.TP_Spring_Belloc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TpSpringBellocApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
